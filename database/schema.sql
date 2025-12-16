c57fd4924a5d6c870f99dd62c70f87ea51bdd06f
lahcen404 <lahcen.maskour2003@gmail.com>
2025-12-16 14:34:53 +0100

    create table users

---
+++ database/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE novacraftdb;
+USE novacraftdb;
+
+-- table users
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+ 
+select * from users;
