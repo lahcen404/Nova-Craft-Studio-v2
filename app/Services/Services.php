0f7539c9b7a6865a75721c7bb28e201effe6295d
lahcen404 <lahcen.maskour2003@gmail.com>
2025-12-21 18:18:58 +0100

    fix urls of imgs

--- app/Services/Services.php
+++ app/Services/Services.php
@@ -5,19 +5,19 @@ return [
         'title' => 'Custom Web Design',
         'description' => 'Creation of unique, high-performing websites tailored to your needs.',
         'price' => '€1200+',
-        'image' => '/assets/imgs/web_design.png' 
+        'image' => 'https://images.ctfassets.net/wp1lcwdav1p1/3yrskNGjruEFcg2pgNJKJz/d5ea2f7a302195ac08038b3e7dd41438/GettyImages-960938238_how_to_become_a_web_developer__1_.jpg?w=1500&h=680&q=60&fit=fill&f=faces&fm=jpg&fl=progressive' 
     ],
     [
         'title' => 'SEO Optimization',
         'description' => 'Improving your search engine rankings to increase your visibility.',
         'price' => '€300/month',
-        'image' => '/assets/imgs/seo_opt.png'
+        'image' => 'https://www.markentive.com/hubfs/agence-seo.jpg'
     ],
     [
         'title' => 'Maintenance and Support',
         'description' => 'Ensure the security and smooth, continuous operation of your website.',
         'price' => '€100/month',
-        'image' => '/assets/imgs/support.png'
+        'image' => 'https://eleks.com/wp-content/uploads/3840x1300_SMaintenance-and-support.jpg'
     ],
 ];
 
